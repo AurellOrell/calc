8dafcce21fdcc997e5bac447ac04a83fa8d09f32
AurellOrell <aur3l163@gmail.com>
2024-03-06 17:00:27 +0700

    update fungsi calc

--- index.cs
+++ index.cs
@@ -10,6 +10,9 @@ namespace HelloWorld
 int b = 6;	  
 Console.Writeline("{0} + {1} = {2}", a, b, plus(a, b));
 Console.Writeline("{0} - {1} = {2}", a, b, minus(a, b));
+
+Console.Writeline("\nTekan sembarang key untuk keluar");
+Console.ReadKey();
     }
 	static int plus(int a, int b){
 	return a + b;
