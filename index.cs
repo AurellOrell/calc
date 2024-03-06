ea94616efaf5f07251cd591a579e8c61c0010b48
AurellOrell <aur3l163@gmail.com>
2024-03-06 16:56:18 +0700

    update fungsi penambahan dan pengurangan

--- index.cs
+++ index.cs
@@ -6,10 +6,17 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");    
+      int a = 10;
+int b = 6;	  
+Console.Writeline("{0} + {1} = {2}", a, b, plus(a, b));
+Console.Writeline("{0} - {1} = {2}", a, b, minus(a, b));
     }
 	static int plus(int a, int b){
-	return a, b;
+	return a + b;
+	}
+	
+	static int plus(int a, int b){
+	return a - b;
 	}
   }
 }
